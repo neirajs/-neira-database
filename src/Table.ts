02bab72e6713a6cf10e412e3fb853898d6af9756
adeyahya <adeyahyaprasetyo@gmail.com>
2020-09-29 07:13:26 +0700

    Object Representation of Table and it's fields

--- src/Table.ts
+++ src/Table.ts
@@ -1,3 +1,26 @@
+import {Field, StringField} from './Field'
+
 export class Table {
-  
+  public name: string;
+  private fields = new Map<string, Field>();  
+
+  public string(name: string): StringField {
+    // prevent duplicate
+    if (this.fields.has(name)) {
+      throw new Error(`duplicate field ${name}`);
+    }
+
+    const field = new StringField(name);
+    this.fields.set(name, field);
+    return field;
+  }
+
+  public toString(): string {
+    const fields = [];
+    this.fields.forEach((field) => {
+      fields.push(field.toString());
+    });
+
+    return fields.join(', ');
+  }
 }
--- tests/Table.spec.ts
+++ tests/Table.spec.ts
@@ -2,10 +2,18 @@ import {expect} from "chai";
 import { Table } from '../src/Table';
 
 describe("Table Test", () => {
-  it("table with string field", () => {
+  it("string field", () => {
     const table = new Table();
-    // @ts-expect-error
-    table.toString('name');
+    table.string('name');
     expect(table.toString()).eq('name VARCHAR(255)')
+    
+    table.string('title').length(20);
+    expect(table.toString()).eq('name VARCHAR(255), title VARCHAR(20)')
+
+    table.string('job_title').primaryKey();
+    expect(table.toString()).eq('name VARCHAR(255), title VARCHAR(20), job_title VARCHAR(255) PRIMARY KEY');
+
+    // TODO: assert error exception
+    // expect(table.string('title')).throw(/duplicate/);
   })
 });
