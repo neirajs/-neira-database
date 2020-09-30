c33da000d01c5ca4eee614aad8c6f27963fe0a7e
adeyahya <adeyahyaprasetyo@gmail.com>
2020-09-30 10:23:50 +0700

    update

--- src/Table.ts
+++ src/Table.ts
@@ -1,26 +1,26 @@
-import {Field, StringField} from './Field'
+import {Field, StringField} from "./Field";
 
 export class Table {
-  public name: string;
-  private fields = new Map<string, Field>();  
+	public name: string;
+	private fields = new Map<string, Field>();
 
-  public string(name: string): StringField {
-    // prevent duplicate
-    if (this.fields.has(name)) {
-      throw new Error(`duplicate field ${name}`);
-    }
+	public string(name: string): StringField {
+		// prevent duplicate
+		if (this.fields.has(name)) {
+			throw new Error(`duplicate field ${name}`);
+		}
 
-    const field = new StringField(name);
-    this.fields.set(name, field);
-    return field;
-  }
+		const field = new StringField(name);
+		this.fields.set(name, field);
+		return field;
+	}
 
-  public toString(): string {
-    const fields = [];
-    this.fields.forEach((field) => {
-      fields.push(field.toString());
-    });
+	public toString(): string {
+		const fields = [];
+		this.fields.forEach((field) => {
+			fields.push(field.toString());
+		});
 
-    return fields.join(', ');
-  }
+		return fields.join(", ");
+	}
 }
--- tests/Table.spec.ts
+++ tests/Table.spec.ts
@@ -1,25 +1,40 @@
-import {assert, expect, should} from "chai";
-import { Table } from '../src/Table';
+import {assert, expect} from "chai";
+import {Table} from "../src/Table";
 
-describe("Table Test", () => {
-  it("string field", () => {
-    const table = new Table();
-    table.string('name');
-    expect(table.toString()).eq('name VARCHAR(255)')
-    
-    table.string('title').length(20);
-    expect(table.toString()).eq('name VARCHAR(255), title VARCHAR(20)')
+describe(
+	"Table Test",
+	() => {
+		it(
+			"string field",
+			() => {
+				const table = new Table();
+				table.string("name");
+				expect(table.toString()).eq("name VARCHAR(255)");
 
-    table.string('job_title').primaryKey();
-    expect(table.toString()).eq('name VARCHAR(255), title VARCHAR(20), job_title VARCHAR(255) PRIMARY KEY');
-    
-    table.string('unique').notNull().unique();
-    expect(table.toString()).eq('name VARCHAR(255), title VARCHAR(20), job_title VARCHAR(255) PRIMARY KEY, unique VARCHAR(255) UNIQUE NOT NULL');
-  })
+				table.string("title").length(20);
+				expect(table.toString()).eq("name VARCHAR(255), title VARCHAR(20)");
 
-  it("duplicate key", () => {
-    const table = new Table();
-    table.string('name');
-    assert.throw(() => { table.string('name') });
-  });
-});
+				table.string("job_title").primaryKey();
+				expect(table.toString()).eq(
+					"name VARCHAR(255), title VARCHAR(20), job_title VARCHAR(255) PRIMARY KEY",
+				);
+
+				table.string("unique").notNull().unique();
+				expect(table.toString()).eq(
+					"name VARCHAR(255), title VARCHAR(20), job_title VARCHAR(255) PRIMARY KEY, unique VARCHAR(255) UNIQUE NOT NULL",
+				);
+			},
+		);
+
+		it(
+			"duplicate key",
+			() => {
+				const table = new Table();
+				table.string("name");
+				assert.throw(() => {
+					table.string("name");
+				});
+			},
+		);
+	},
+);
